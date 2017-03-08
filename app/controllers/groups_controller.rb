5c9d0d5592df23285defaad3c94e33999f3e9518
donker008 <richdonker@qq.com>
2017-03-08 17:04:04 +0800

    add groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,21 @@
 class GroupsController < ApplicationController
 
   def index
-    @groups = Group.all  
+    @groups = Group.all
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(post_params)
+    @group.save
+    redirect_to groups_path
+  end
+
+ private
+ def post_params
+   params.require(:group).permit(:title,:description)
+ end
 end
