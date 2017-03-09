45d18dee59c8d5e9610f6023fee8f0954279e709
donker008 <richdonker@qq.com>
2017-03-09 11:24:05 +0800

    add is_member_of?

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   has_many :posts
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
+
+  def is_member_of?(group)
+    if participated_groups.include?(group)
+      return true
+    else
+      return false
+    end
+  end
 end
