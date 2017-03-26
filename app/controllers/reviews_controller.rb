5177daece5dfda17b9e9417d36f241fd8bb77f81
donker008 <richdonker@qq.com>
2017-03-27 07:12:25 +0800

    implement write review for movie

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,29 @@ class ReviewsController < ApplicationController
     @movie = Movie.find(params[:movie_id])
   end
 
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.new
+    @review.movie = Movie.find(params[:movie_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.movie = Movie.find(params[:movie_id])
+    @review.user = current_user
+    if @review.save
+      flash[:notice] = "Review successful!"
+      redirect_to movie_reviews_path
+    else
+      flash[:error] = "Review failed!"
+      render :new
+    end
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
